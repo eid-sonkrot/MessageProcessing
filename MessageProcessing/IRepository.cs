a6a46a1620d5693c20bca9dabd941a47b621f7bf
Smile <Smile@DESKTOP-NOGL6KQ>
2023-10-15 23:09:15 +0300

    [Database] immplement database functinalty

---
+++ MessageProcessing/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageProcessing
+{
+    public interface IRepository
+    {
+        Task InsertAsync(ServerStatistics entity);
+        Task<ServerStatistics> GetByIdAsync(Guid id);
+        Task<IEnumerable<ServerStatistics>> GetAllAsync();
+        Task UpdateAsync(Guid id, ServerStatistics entity);
+        Task DeleteAsync(Guid id);
+    }
+}
